c8492afe5777cd1ba2a80d40c8af18115fcf4fe8
primrataqz <primrataq@gmail.com>
2026-07-12 05:31:18 +0000

    f

--- test.py
+++ test.py
@@ -148,7 +148,7 @@ class Pricing(PaidTemplate):
             alignment=r"\flushleft",
             font_size=36,
         )
-        pricing.next_to(pricing, DOWN, buff=MED_LARGE_BUFF)
+        pricing.next_to(pt_underline, DOWN, buff=MED_LARGE_BUFF)
         self.add(pricing)
 
         best = TexText(
