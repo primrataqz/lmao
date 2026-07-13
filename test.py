73d1c29fc94561533affb7345e36776a3ec1e40f
primrataqz <primrataq@gmail.com>
2026-07-13 07:53:19 +0000

    f

--- test.py
+++ test.py
@@ -223,7 +223,7 @@ class Dis(PaidTemplate):
     pgno = "5"
 
     def construct(self):
-        t = TexText("Disclaimer:").shift(RIGHT * 0.1)
+        t = TexText("Disclaimer:").shift(RIGHT * 0.25)
         t.next_to(self.logo, DOWN, buff=MED_LARGE_BUFF, aligned_edge=LEFT)
         under = Underline(t, stroke_width=[0.5, 1.5, 1.5, 0.5])
         self.add(t, under)
