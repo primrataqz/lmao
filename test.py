d134691795173558af150d585b9fc2adef24effb
primrataqz <primrataq@gmail.com>
2026-07-13 07:12:04 +0000

    f

--- test.py
+++ test.py
@@ -50,7 +50,8 @@ class Paid(PaidTemplate):
         self.add(cont, cont_underline)
 
         cont1 = TexText("200GB+ Curated Educational Library", font_size=30)
-        cont1.next_to(cont_underline, DOWN, buff=MED_LARGE_BUFF * 0.45, aligned_edge=LEFT)
+        cont1.next_to(cont_underline, DOWN, buff=MED_LARGE_BUFF * 0.45)
+        cont1.to_edge(RIGHT)
         self.add(cont1)
 
         itm = BulletedList(
