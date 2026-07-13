bf5f343f71a4cf3d5809d041d85a8eef45438d2b
primrataqz <primrataq@gmail.com>
2026-07-13 07:16:43 +0000

    f

--- test.py
+++ test.py
@@ -46,7 +46,7 @@ class Paid(PaidTemplate):
     def construct(self):
         cont = TexText(R"The Knowledge Vault")
         cont.next_to(self.logo, DOWN, buff=LARGE_BUFF * 0.5, aligned_edge=LEFT)
-        cont.shift(RIGHT * 0.3)
+        cont.shift(RIGHT * 0.35)
         cont_underline = Underline(cont, stroke_width=[0.5, 2, 2, 0.5])
         self.add(cont, cont_underline)
 
